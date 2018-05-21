b7b5b3ff266edbc4fda74c288969b34655a2bd74
Shwetajain148 <sjain@loggly.com>
2018-05-21 15:30:39 +0530

    new file

---
+++ test/winston-test.js
@@ -0,0 +1,49 @@
+const winston  = require('winston');
+const {Loggly} = require('winston-loggly-bulk');
+
+winston.add(new Loggly({
+    token: "72ba7555-ba9d-4f90-bc62-20ac39afebfe",
+    subdomain: "mylabenv",
+    tags: ["NodeJS-V3-all-Input"],
+    //isBulk: true,
+    json: true
+}), new (winston.transports.Console)());
+
+var source = {
+  foo: 1,
+  bar: 2,
+  buzz: {
+      sheep: 'jumped',
+      times: 10
+  }
+};
+
+winston.log('info', '');
+winston.log('info', null);
+
+try {
+  var a = 10, b = 0;
+  a = a / b;
+  throw new Error("Something unexpected has occurred.", a);
+
+} catch (e) {
+  winston.error(e);
+}
+winston.log('info', 'cry me a river :(');
+winston.log('info', source);
+
+// setInterval(function () {
+//   sendMultipleLogs();
+// }, 5 * 1000);
+
+// function sendMultipleLogs() {
+//   for (i = 0; i < 100; i++) {
+//       //winston.log('info', source);
+//       winston.log('info', 'cry me a river :(');
+//       winston.log('info', source);
+//   }
+// }
+
+for (i = 0; i < 20; i++) {
+    winston.log('info', "Hello World from Node.js!", i);
+}
