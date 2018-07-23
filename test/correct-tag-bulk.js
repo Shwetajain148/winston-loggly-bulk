64121a96154e423b676d73be0f219e2224310e72
Shwetajain148 <sjain@loggly.com>
2018-07-23 13:13:25 +0530

    correct-tag-bulk

---
+++ test/correct-tag-bulk.js
@@ -0,0 +1,54 @@
+const winston  = require('winston');
+require('..');
+
+winston.add(winston.transports.Loggly, {
+    token: "72ba7555-ba9d-4f90-bc62-20ac39afebfe",
+    subdomain: "mylabenv",
+    tags: ["correct-tag-bulk"],
+    json: true,
+    isBulk: true,
+    bufferOptions: {
+        size: 500,
+        retriesInMilliSeconds: 1 * 5000,
+
+    },
+});
+
+var source = {
+  foo: 1,
+  bar: 2,
+  buzz: {
+      sheep: 'jumped',
+      times: 10
+  }
+};
+
+winston.log('info', '');
+winston.log('info', null);
+
+try {
+  var a = 10, b = 0;
+  a = a / b;
+  throw new Error("Something unexpected has occurred.", a);
+
+} catch (e) {
+  winston.error(e);
+}
+winston.log('info', 'cry me a river :(');
+winston.log('info', source);
+
+ setInterval(function () {
+   sendMultipleLogs();
+ }, 5 * 1000);
+
+ function sendMultipleLogs() {
+   for (i = 0; i < 100; i++) {
+       //winston.log('info', source);
+       winston.log('info', 'cry me a river :(');
+       winston.log('info', source);
+   }
+ }
+
+for (i = 0; i < 20; i++) {
+    winston.log('info', "Hello World from Node.js!", i);
+}
